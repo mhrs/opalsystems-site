494b616d143257e4b4a90561031230f1791846a2
mhrs <m.h.0704@gmail.com>
2026-03-23 00:08:58 +0900

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Opal Systems",
+  description: "Mind modeling for everyday life",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
